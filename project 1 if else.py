c09a1bb80c2c88b3f74329e9164ea5a798fc6c60
wantyaayu <150386256+wantyaayu@users.noreply.github.com>
2023-11-10 22:02:01 +0700

    Add files via upload
    TUGAS UTS ALGORITMA PEMOGRAMAN SEMESTER 1

---
+++ project 1 if else.py
@@ -0,0 +1,25 @@
+nilai_ujian = int(input('masukan nilai ujian: '))
+ 
+if 90 <= nilai_ujian <= 100:
+    print("Selamat, Anda mendapatkan nilai A.")
+elif 70 <= nilai_ujian <= 80:
+    print("Anda mendapatkan nilai B.")
+else:
+    print("Anda mendapat nilai c.")
+ 
+#tambahan keadaan lain
+ 
+if nilai_ujian >= 70:
+     print('selamat kamu lulus ujian')
+else:
+    print('anda tidak lulus ujian')
+ 
+# tambahan
+if 90 <= nilai_ujian <= 100 : 
+    print('great job')
+elif 70 <= nilai_ujian <= 80:
+    print('good job, ayo berusaha lagi')
+else: 
+    print('coba di lain kesempatan')
+
+
